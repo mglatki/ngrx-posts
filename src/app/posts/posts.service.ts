140fdf80116be9b39e026134ac63ece25928c495
mglatki <mglatki@gmail.com>
2021-12-08 00:21:51 +0100

    Created methot to get posts from API

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { Post } from '../models/posts';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostsService {
+  postsUrl = `https://jsonplaceholder.typicode.com/posts`;
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.postsUrl);
+  }
 }
