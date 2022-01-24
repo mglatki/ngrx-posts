da00c8dcf6ba7648c40fa2dc273cfd32507a21c8
mglatki <mglatki@gmail.com>
2022-01-24 13:28:34 +0100

    API url moved to evironment.ts file

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -3,12 +3,13 @@ import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 import { Post } from '../models/posts';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostsService {
-  postsUrl = `https://jsonplaceholder.typicode.com/posts`;
+  postsUrl = environment.postsUrl;
   constructor(private http: HttpClient) {}
 
   getPosts(): Observable<Post[]> {
